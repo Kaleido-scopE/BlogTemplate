b19d23197a24c2a82dd87e1e81306b3df779c9b4
NoahSun <873717994@qq.com>
2019-01-14 15:34:59 +0800

    使用了打包的SDK

--- src/sdk/utils/encoder/CEStreamExhausted.java
+++
@@ -1,8 +0,0 @@
-package sdk.utils.encoder;
-
-import java.io.IOException;
-
-public class CEStreamExhausted extends IOException
-{
-
-}
--- src/utils/SMSManager.java
+++ src/utils/SMSManager.java
@@ -1,6 +1,8 @@
 package utils;
 
-import sdk.CCPRestSDK;
+
+
+import com.cloopen.rest.sdk.CCPRestSmsSDK;
 
 import java.util.HashMap;
 import java.util.Random;
@@ -19,7 +21,7 @@ public class SMSManager {
     public String sendMessage() {
         String verificationCode = String.valueOf((int)((codeGenerator.nextDouble()*9+1)*10000));
 
-        CCPRestSDK restAPI = new CCPRestSDK();
+        CCPRestSmsSDK restAPI = new CCPRestSmsSDK();
         restAPI.init("app.cloopen.com", "8883");// 初始化服务器地址和端口，格式如下，服务器地址不需要写https://
         restAPI.setAccount("8a216da86812593601684b043b8219a1", "de612d08507d4f6689a410449070623d");// 初始化主帐号和主帐号TOKEN
         restAPI.setAppId("8a216da86812593601684b043bd419a7");// 初始化应用ID
@@ -31,7 +33,7 @@ public class SMSManager {
     }
 
     public static void main(String[] args) {
-        SMSManager manager = new SMSManager("15526088820", "1");
+        SMSManager manager = new SMSManager("18374803700", "1");
         System.out.println(manager.sendMessage());
     }
 }
